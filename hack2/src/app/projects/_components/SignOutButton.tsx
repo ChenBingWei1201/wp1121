90d3ce4330e8c7b73250e4ee9b667a09c05cdbb3
ChenBingWei1201 <rain4030633@gmail.com>
2023-11-23 09:58:24 +0800

    1st test

--- hack2/src/app/projects/_components/SignOutButton.tsx
+++ hack2/src/app/projects/_components/SignOutButton.tsx
@@ -1,3 +1,4 @@
+"use client";
 // TODO: 4. Call the signOut() function when the button is clicked
 // hint: You may want to change the first line of this file
 import { signOut } from "next-auth/react";
@@ -5,6 +6,6 @@ import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 
 export default function SignOutButton() {
-  return <Button variant={"outline"}>Sign Out</Button>;
+  return <Button variant={"outline"} onClick={() => signOut({ callbackUrl: process.env.NEXT_PUBLIC_BASE_URL})}>Sign Out</Button>;
 }
 // TODO: 4. end
